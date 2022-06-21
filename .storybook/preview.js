8eadd05e708530e597afff7e4d7d96d55ac81e6b
Mark <mark@favisbook.com>
2022-06-21 11:50:54 -0400

    accessibility addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,6 +18,13 @@ export const decorators = [
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  // Storybook a11y addon config
+  a11y: {
+    // target DOM element
+    element: "#root",
+    // set exclusion mode for addon
+    manual: false,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
